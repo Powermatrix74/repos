182919a44918445ee4cf09a00b2d3c63d26ad841
Kay Storto <kay.storto@gmail.com>
2019-12-22 19:19:05 +0100

    added nancy web framework for testing purposes

---
+++ ReadGeoApi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ReadGeoApi;
+
+namespace ReadGeoApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeoApiHelper geoApi = new GeoApiHelper();
+            var result = geoApi.ReadSingleValue();
+            result.Wait();
+            var returned = result.Result;
+        }
+    }
+}
