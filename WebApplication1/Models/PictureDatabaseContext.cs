40d2ece9678c943c47768e6c1048bd98fe64dffc
Kay Storto <kay.storto@local.ch>
2020-07-31 15:39:39 +0200

    Picture Database added

---
+++ WebApplication1/Models/PictureDatabaseContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MyWebAppGit.Models
+{
+    public partial class PictureDatabaseContext : DbContext
+    {
+        public PictureDatabaseContext()
+        {
+        }
+
+        public PictureDatabaseContext(DbContextOptions<PictureDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<PictureInformation> PictureInformation { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=KAY-PC\\KAYSQLEXPRESS;Database=PictureDatabase;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PictureInformation>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.PicturePath)
+                    .IsRequired()
+                    .HasMaxLength(500);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ WebApplication1/Models/PictureInformation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyWebAppGit.Models
+{
+    public partial class PictureInformation
+    {
+        public int Id { get; set; }
+        public string PicturePath { get; set; }
+    }
+}
